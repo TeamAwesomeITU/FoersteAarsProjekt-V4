9caab7989fa6f748dce0c38b3916fac47e25eacc
jnysteen <jespernysteen@gmail.com>
2013-03-15 00:13:41 +0100

    nu med mapDrawer + lidt indhold

---
+++ src/mapDrawer/Edge.java
@@ -0,0 +1,49 @@
+package mapDrawer;
+
+public class Edge {
+	
+	private int fromNode;
+	private int toNode;
+	private int roadType;
+	private String roadName;
+	//Postal number for the right side of the road!
+	private int postalNumber;
+	
+	public Edge(int fromNode, int toNode, int roadType, String roadName, int postalNumber)
+	{
+		this.fromNode = fromNode;
+		this.toNode = toNode;
+		this.roadType = roadType;
+		this.roadName = roadName;
+		this.postalNumber = postalNumber;
+	}
+	
+	public int getFromNode()
+	{
+		return fromNode;
+	}
+	
+	public int getToNode()
+	{
+		return toNode;
+	}
+
+	
+	public int getRoadType()
+	{
+		return roadType;
+	}
+
+	
+	public String getRoadName()
+	{
+		return roadName;
+	}
+
+	
+	public int getPostalNumber()
+	{
+		return postalNumber;
+	}
+
+}
---
+++ src/mapDrawer/ZoomLevel.java
@@ -0,0 +1,14 @@
+package mapDrawer;
+
+public enum ZoomLevel {
+	
+	//Which roadType to paint
+	ONE(1), TWO(2), THREE(3), FOUR(4), 
+	FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10);
+	private int value;
+	
+	private ZoomLevel(int value) 
+	{
+		this.value = value;
+	}	
+}
