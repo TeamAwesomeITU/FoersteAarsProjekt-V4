15807c214013d829474f3f2697166ecd08600a66
FrederikTH <Frederikhoffmann1@gmail.com>
2013-03-18 12:08:19 +0100

    Med NodeXML

--- src/xmlClasses/VTDfjol.java
+++ src/xmlClasses/VTDfjol.java
@@ -10,7 +10,8 @@ public class VTDfjol {
 public static void main(String[] args) throws VTDException {
 		
 		try {
-			parse();
+			parseNode();
+			//parseEdge();
 		} catch (NavException | XPathParseException
 				| XPathEvalException | ParseException | IOException e) {
 			
@@ -18,38 +19,79 @@ public static void main(String[] args) throws VTDException {
 		}
 		
 	}
-	
-	public static void parse() throws IOException, EncodingException, VTDException, EntityException, ParseException {
-	File f = new File("kdv_unload_omskaaretEdition.xml");
-	FileInputStream fis = new FileInputStream(f);
-	byte[] b = new byte[(int)f.length()];
-	fis.read(b);
 
-	VTDGen vg = new VTDGen();
-	vg.setDoc(b);
-	vg.parse(true);
+	public void VTD() {
+		
+	}
 	
-	VTDNav vn = vg.getNav();
-	AutoPilot ap = new AutoPilot(vn);
+	public static void parseEdge() throws IOException, EncodingException, VTDException, EntityException, ParseException {
+	File fi = new File("XML/kdv_unload_omskaaretEdition.xml");
+	FileInputStream fis = new FileInputStream(fi);
+	byte[] b = new byte[(int)fi.length()];
+	fis.read(b);
 	
-	ap.declareXPathNameSpace("", "");
-	//bookstore/book[price>35.00]/title
-	ap.selectXPath("//roadSegmentCollection/roadSegment[TYP<2 and FNODE=490165]/*"); 
+	VTDGen vgEdge = new VTDGen();
+	vgEdge.setDoc(b);
+	vgEdge.parse(true);
+
+	VTDNav vnEdge = vgEdge.getNav();
+	AutoPilot apEdge = new AutoPilot(vnEdge);
+	
+	apEdge.declareXPathNameSpace("", "");
+	apEdge.selectXPath("//roadSegmentCollection/roadSegment[TYP<2 and FNODE=490165]/*"); 
+	
+	
 	
 	int result = -1;
 	int count = 0;
-	while ((result=ap.evalXPath())!=-1) {
+	while ((result=apEdge.evalXPath())!=-1) {
 		System.out.println(""+result+"");
-		System.out.println("Element name ==> "+vn.toString(result));
-		int t = vn.getText();
+		System.out.println("Element name ==> "+vnEdge.toString(result));
+		int t = vnEdge.getText();
 		// String temp = vn.toNormalizedString(t); 
-		 if(t!=-1 && vn.toNormalizedString(t) != "1")			  
-			System.out.println("Text ==>"+vn.toNormalizedString(t));
+		 if(t!=-1 && vnEdge.toNormalizedString(t) != "1")			  
+			System.out.println("Text ==>"+vnEdge.toNormalizedString(t));
 		System.out.println("\n====================================");
 		count++;
 	}
 	System.out.println("Total# of element"+count);
 	fis.close();
-	}
-	
+	}	
+
+
+	public static void parseNode() throws IOException, EncodingException, VTDException, EntityException, ParseException {
+		File fo = new File("XML/kdv_node_unload.xml");
+		FileInputStream fos = new FileInputStream(fo);
+		byte[] n = new byte[(int)fo.length()];
+		fos.read(n);
+
+		VTDGen vgNode = new VTDGen();
+		vgNode.setDoc(n);
+		vgNode.parse(true);
+
+		VTDNav vnNode = vgNode.getNav();
+		AutoPilot apNode = new AutoPilot(vnNode);
+		
+		int Xma=490000; int Xmi=480000; int Yma=7000000; int Ymi=6000000;
+
+		apNode.declareXPathNameSpace("", "");
+		apNode.selectXPath("//nodeCollection/node[X-COORD < "+Xma+" and X-COORD > "+Xmi+" and Y-COORD < "+Yma+" and Y-COORD > "+Ymi+"]/*");
+
+
+		int result = -1;
+		int count = 0;
+		while ((result=apNode.evalXPath())!=-1) {
+			//System.out.println(""+result+"");
+			//System.out.println("Element name ==> "+vnNode.toString(result));
+			int t = vnNode.getText();
+	// String temp = vn.toNormalizedString(t); 
+			if(t!=-1 && vnNode.toNormalizedString(t) != "1")			  
+				//System.out.println("Text ==>"+vnNode.toNormalizedString(t));
+			//System.out.println("\n====================================");
+			count++;
+		}
+		System.out.println("Total# of element"+count);
+		fos.close();
+	}	
 }
+
