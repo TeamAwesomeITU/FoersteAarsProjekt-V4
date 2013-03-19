23f00b6580275096c0b27efbc8124195684e246b
Jesper <jespernysteen@gmail.com>
2013-03-19 12:45:38 +0100

    så guys

--- src/mapDrawer/FindRelevantNodes.java
+++ src/mapDrawer/FindRelevantNodes.java
@@ -26,7 +26,7 @@ public class FindRelevantNodes {
 	private static HashSet<String> findNodes(AreaToDraw area)
 	{
 		HashSet<String> nodeSet = new HashSet<String>();
-
+		long startTime = System.currentTimeMillis();
 		try {
 			File fo = new File("XML/kdv_node_unload.xml");
 			FileInputStream fos = new FileInputStream(fo);
@@ -35,46 +35,40 @@ public class FindRelevantNodes {
 
 			VTDGen vgNode = new VTDGen();
 			vgNode.setDoc(n);
-			vgNode.parse(true);
+			vgNode.parse(false);
 
 			VTDNav vnNode = vgNode.getNav();
 			AutoPilot apNode = new AutoPilot(vnNode);
 
-			double Xma = area.getLargestX();
-			double Xmi = area.getSmallestX();
-			double Yma = area.getLargestY();
-			double Ymi = area.getSmallestY();
+			double Xmax = area.getLargestX(); double Xmi = area.getSmallestX();
+			double Ymax = area.getLargestY(); double Ymin = area.getSmallestY();
 
-			apNode.declareXPathNameSpace("", "");
-			apNode.selectXPath("//nodeCollection/node[X-COORD < "+Xma+" and X-COORD > "+Xmi+" and Y-COORD < "+Yma+" and Y-COORD > "+Ymi+"]/KDV");
+			apNode.selectXPath("//nodeCollection/node[X-COORD < "+Xmax+" and X-COORD > "+Xmi+" and Y-COORD < "+Ymax+" and Y-COORD > "+Ymin+"]/KDV");
 
 			int count = 0;
 			while (apNode.evalXPath() != -1) {
 				int t = vnNode.getText();
-
-				if(t!=-1 && vnNode.toNormalizedString(t) != "1")
+				if(t!=-1 && vnNode.toNormalizedString(t) != "1")	{
 					nodeSet.add(vnNode.toNormalizedString(t));
-
+				}
 				count++;
 			}
 			System.out.println("Total# of element "+count);
 			fos.close();
-
 			System.out.println("Size of nodeSet " + nodeSet.size());
-
 		} catch (IOException | VTDException e){
 			e.printStackTrace();
 		}
-
+		long endTime = System.currentTimeMillis();
+		System.out.println("nodeSet tager " + (endTime - startTime) + " milliseconds");
 		return nodeSet;
-
 	}
 
 	private static HashSet<Edge> findEdges(AreaToDraw area, HashSet<String> nodeSet)
 	{
+		long startTime = System.currentTimeMillis();
 		HashSet<Edge> edgeSet = new HashSet<Edge>();
-
-		try {
+		try {			
 			area.getPercentageOfEntireMap();
 
 			File fi = new File("XML/kdv_unload_omskaaretEdition.xml");
@@ -88,52 +82,54 @@ public class FindRelevantNodes {
 
 			VTDNav vnEdge = vgEdge.getNav();
 			AutoPilot apEdge = new AutoPilot(vnEdge);
-			apEdge.declareXPathNameSpace("", "");
-			
-			int edgeObjectCount = 0;
-			int count = 0;
-			int type = 1;
-			String node = null;
+
+			int edgeObjectCount = 0; int count = 0; int type = 44;		
 			String output = null;
 			String[] edgeInfoArray = new String[5];
 
-			Iterator<String> iterator = nodeSet.iterator();			
-			while(iterator.hasNext()) {
-				node = iterator.next();
-				edgeObjectCount = 0;
-				apEdge.selectXPath("//roadSegmentCollection/roadSegment[TYP <= 4 and FNODE=539147 or TNODE = 538398]/*"); 
-				while ((apEdge.evalXPath())!=-1) {
-					
-					int t = vnEdge.getText();
-					if(t!=-1 && vnEdge.toNormalizedString(t) != "1")			  
-						output = vnEdge.toNormalizedString(t);
-					edgeInfoArray[edgeObjectCount] = output;
-					edgeObjectCount++;
-					
-					if(edgeObjectCount == 5)
-					{
-						edgeObjectCount = 0;
+			apEdge.selectXPath("//roadSegmentCollection/roadSegment[TYP <= "+type+"]/*"); 
+			while ((apEdge.evalXPath())!=-1) {
+
+				int t = vnEdge.getText();
+				if(t!=-1 && vnEdge.toNormalizedString(t) != "1")	{			  
+					output = vnEdge.toNormalizedString(t);
+				}
+				edgeInfoArray[edgeObjectCount] = output;
+				edgeObjectCount++;
+				if(edgeObjectCount == 5)	{
+					edgeObjectCount = 0;
+					if(nodeSet.contains(edgeInfoArray[0]) || nodeSet.contains(edgeInfoArray[1]))		{
 						edgeSet.add(new Edge(Integer.parseInt(edgeInfoArray[0]), Integer.parseInt(edgeInfoArray[1]), 
 								Integer.parseInt(edgeInfoArray[2]), edgeInfoArray[3], Integer.parseInt(edgeInfoArray[4])));
-						System.out.println("LOL");
 						count++;
 					}
-					
 				}
-				
-				//System.out.println("Total# of element"+count);
+
+
 			}		
+			System.out.println("Total# of elements "+count);
 			fis.close();
 		}
 		catch (IOException | VTDException e){
 			e.printStackTrace();
 		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("EdgeHalløj tager " + (endTime - startTime) + " milliseconds");
+		System.out.println("Done");
 		return edgeSet;
 	}
 
 	public static void main(String[] args)
 	{		
-		findNodesToDraw(new AreaToDraw());		
-	}
+		HashSet<Edge> set = findNodesToDraw(new AreaToDraw());		
+		/*
+		Iterator<Edge> iterator = set.iterator();
+		Edge edge = null;
 
+		while (iterator.hasNext()) {
+			edge = iterator.next();
+			System.out.println(edge.getRoadName());			
+		}
+		 */
+	}
 }
