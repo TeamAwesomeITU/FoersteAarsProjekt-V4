c615c9af913c08753bda1049537a0d2763a3683e
FrederikTH <Frederikhoffmann1@gmail.com>
2013-03-25 10:28:28 +0100

    wsdaas

--- src/mapDrawer/FindRelevantNodes.java
+++ src/mapDrawer/FindRelevantNodes.java
@@ -1,13 +1,8 @@
 package mapDrawer;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.Iterator;
-
 import com.ximpleware.AutoPilot;
-
 import com.ximpleware.VTDException;
 import com.ximpleware.VTDGen;
 import com.ximpleware.VTDNav;
@@ -16,120 +11,145 @@ import com.ximpleware.VTDNav;
  */
 public class FindRelevantNodes {
 
+	private static final VTDNav vnEdge = indexEdgeLoad();
+	private static final VTDNav vnNode = indexNodeLoad();
+
 	public static HashSet<Edge> findNodesToDraw(AreaToDraw area)
 	{
 		//Doesn't use a HashSet of Integer, since XPath uses Strings as "input"
-		HashSet<String> nodeSet = findNodes(area);
+		HashSet<Integer> nodeSet = findNodes(area);
 		return findEdges(area, nodeSet);
 	}
 
-	private static HashSet<String> findNodes(AreaToDraw area)
+	private static HashSet<Integer> findNodes(AreaToDraw area)
 	{
-		HashSet<String> nodeSet = new HashSet<String>();
-		long startTime = System.currentTimeMillis();
+		HashSet<Integer> nodeSet = new HashSet<Integer>();
 		try {
-			File fo = new File("XML/kdv_node_unload.xml");
-			FileInputStream fos = new FileInputStream(fo);
-			byte[] n = new byte[(int)fo.length()];
-			fos.read(n);
+			long startTime = System.currentTimeMillis();
+			VTDNav vn = vnNode;			
+			AutoPilot ap = new AutoPilot(vn);
+			ap.selectXPath("//nodeCollection/node");
 
-			VTDGen vgNode = new VTDGen();
-			vgNode.setDoc(n);
-			vgNode.parse(false);
-
-			VTDNav vnNode = vgNode.getNav();
-			AutoPilot apNode = new AutoPilot(vnNode);
-
-			double Xmax = area.getLargestX(); double Xmi = area.getSmallestX();
+			double Xmax = area.getLargestX(); double Xmin = area.getSmallestX();
 			double Ymax = area.getLargestY(); double Ymin = area.getSmallestY();
+			double YCOORD = 0.0; double XCOORD = 0.0; int count =0;
 
-			apNode.selectXPath("//nodeCollection/node[X-COORD < "+Xmax+" and X-COORD > "+Xmi+" and Y-COORD < "+Ymax+" and Y-COORD > "+Ymin+"]/KDV");
-
-			int count = 0;
-			while (apNode.evalXPath() != -1) {
-				int t = vnNode.getText();
-				if(t!=-1 && vnNode.toNormalizedString(t) != "1")	{
-					nodeSet.add(vnNode.toNormalizedString(t));
+			while (ap.evalXPath() != -1) {
+				vn.toElement(VTDNav.FIRST_CHILD, "Y-COORD"); 
+				YCOORD = vn.parseDouble(vn.getText());
+				if(YCOORD < Ymax && YCOORD > Ymin) {
+					vn.toElement(VTDNav.PREV_SIBLING, "X-COORD"); 
+					XCOORD = vn.parseDouble(vn.getText());
+					if(XCOORD < Xmax && XCOORD > Xmin) {					
+						vn.toElement(VTDNav.PREV_SIBLING, "KDV");				
+						nodeSet.add(vn.parseInt(vn.getText()));
+						count++;
+					}
 				}
-				count++;
+				vn.toElement(VTDNav.PARENT);
 			}
-			System.out.println("Total# of element "+count);
-			fos.close();
-			System.out.println("Size of nodeSet " + nodeSet.size());
-		} catch (IOException | VTDException e){
+			System.out.println("Number of nodes sent to edges "+ count);
+			long endTime = System.currentTimeMillis();
+			System.out.println("Confirm Node tager " + (endTime - startTime) + " milliseconds");
+
+		} catch (VTDException e){
 			e.printStackTrace();
 		}
-		long endTime = System.currentTimeMillis();
-		System.out.println("nodeSet tager " + (endTime - startTime) + " milliseconds");
+		System.out.println("___________________________________________");
 		return nodeSet;
+
 	}
 
-	private static HashSet<Edge> findEdges(AreaToDraw area, HashSet<String> nodeSet)
+	private static HashSet<Edge> findEdges(AreaToDraw area, HashSet<Integer> nodeSet)
 	{
 		long startTime = System.currentTimeMillis();
 		HashSet<Edge> edgeSet = new HashSet<Edge>();
 		try {			
-			area.getPercentageOfEntireMap();
-
-			File fi = new File("XML/kdv_unload_omskaaretEdition.xml");
-			FileInputStream fis = new FileInputStream(fi);
-			byte[] b = new byte[(int)fi.length()];
-			fis.read(b);
-
-			VTDGen vgEdge = new VTDGen();
-			vgEdge.setDoc(b);
-			vgEdge.parse(true);
-
-			VTDNav vnEdge = vgEdge.getNav();
-			AutoPilot apEdge = new AutoPilot(vnEdge);
-
-			int edgeObjectCount = 0; int count = 0; int type = 44;		
-			String output = null;
-			String[] edgeInfoArray = new String[5];
-
-			apEdge.selectXPath("//roadSegmentCollection/roadSegment[TYP <= "+type+"]/*"); 
-			while ((apEdge.evalXPath())!=-1) {
-
-				int t = vnEdge.getText();
-				if(t!=-1 && vnEdge.toNormalizedString(t) != "1")	{			  
-					output = vnEdge.toNormalizedString(t);
-				}
-				edgeInfoArray[edgeObjectCount] = output;
-				edgeObjectCount++;
-				if(edgeObjectCount == 5)	{
-					edgeObjectCount = 0;
-					if(nodeSet.contains(edgeInfoArray[0]) || nodeSet.contains(edgeInfoArray[1]))		{
-						edgeSet.add(new Edge(Integer.parseInt(edgeInfoArray[0]), Integer.parseInt(edgeInfoArray[1]), 
-								Integer.parseInt(edgeInfoArray[2]), edgeInfoArray[3], Integer.parseInt(edgeInfoArray[4])));
-						count++;
+			if(nodeSet.isEmpty()){
+				System.out.println("No nodes within the given coordinates");
+			}
+			else {							
+				VTDNav vn = vnEdge;
+				System.out.println("DONE LOADING");
+				AutoPilot ap = new AutoPilot(vn); 
+				int type = 2;
+				ap.selectXPath("//roadSegmentCollection/roadSegment[TYP <= "+type+"]");							
+				String FNODE = ""; String TNODE = ""; 
+				int TYP = 0;   String ROAD = ""; 
+				int POST = 0;
+				int count1 = 0;
+				while((ap.evalXPath())!=-1)
+				{
+					vn.toElement(VTDNav.FIRST_CHILD, "FNODE");
+					FNODE = vn.toString(vn.getText());
+					vn.toElement(VTDNav.NEXT_SIBLING, "TNODE");
+					TNODE = vn.toString(vn.getText());
+					count1++;
+					if(nodeSet.contains(FNODE)||nodeSet.contains(TNODE)) {
+						vn.toElement(VTDNav.NEXT_SIBLING, "TYP");
+						TYP = vn.parseInt(vn.getText());
+						vn.toElement(VTDNav.NEXT_SIBLING, "VEJNAVN");
+						ROAD = vn.toString(vn.getText());
+						vn.toElement(VTDNav.NEXT_SIBLING, "H_POSTNR");
+						POST = vn.parseInt(vn.getText());
+						edgeSet.add(new Edge(Integer.parseInt(FNODE),Integer.parseInt(TNODE),TYP,ROAD,POST));				
 					}
-				}
-
-
-			}		
-			System.out.println("Total# of elements "+count);
-			fis.close();
+					vn.toElement(VTDNav.PARENT); 
+				} 
+				ap.resetXPath();
+				System.out.println("Number of FNODES and TNODES Checked "+count1);				
+			}
 		}
-		catch (IOException | VTDException e){
+		catch (VTDException e){
 			e.printStackTrace();
 		}
 		long endTime = System.currentTimeMillis();
-		System.out.println("EdgeHalløj tager " + (endTime - startTime) + " milliseconds");
-		System.out.println("Done");
+		System.out.println("Confirm Edge takes " + (endTime - startTime) + " milliseconds");
+		System.out.println("Edges sent " + edgeSet.size());
+		System.out.println("Done with edges");
+		System.out.println("___________________________________________");
 		return edgeSet;
-	}
+	} 
 
 	public static void main(String[] args)
 	{		
-		HashSet<Edge> set = findNodesToDraw(new AreaToDraw());		
-		/*
-		Iterator<Edge> iterator = set.iterator();
-		Edge edge = null;
-
-		while (iterator.hasNext()) {
-			edge = iterator.next();
-			System.out.println(edge.getRoadName());			
-		}
-		 */
+		long startTime = System.currentTimeMillis();
+		findNodesToDraw(new AreaToDraw());		
+		long endTime = System.currentTimeMillis();
+		System.out.println("Total " + (endTime - startTime) + " milliseconds");
+		System.out.println("___________________________________________");
 	}
+	
+	private static VTDNav indexEdgeLoad(){
+		long startTime = System.currentTimeMillis();
+		VTDNav vn = null;
+
+		try{
+			VTDGen vg = new VTDGen();
+			vn = vg.loadIndex("XML/edge_unload.vxl");
+		} catch(IOException | VTDException e) {
+			e.printStackTrace();
+
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("Load Edge Index: " + (endTime - startTime) + " milliseconds");
+		return vn;
+	}
+
+	private static VTDNav indexNodeLoad() {
+		long startTime = System.currentTimeMillis();
+		VTDNav vn = null;
+
+		try{
+			VTDGen vg = new VTDGen();
+			vn = vg.loadIndex("XML/Node_unload.vxl");
+		} catch(IOException | VTDException e) {
+			e.printStackTrace();
+
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("Load Node Index " + (endTime - startTime) + " milliseconds");
+		return vn;
+	}	
+
 }
