0d74dd8d2956e71993cba9d870a9740e5d128ff8
Jesper <jespernysteen@gmail.com>
2013-03-25 13:49:33 +0100

    nu med fungerende zoomLevel!

--- src/mapDrawer/MapPanel.java
+++ src/mapDrawer/MapPanel.java
@@ -1,12 +1,10 @@
 package mapDrawer;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.geom.Line2D;
 import java.io.BufferedReader;
--- src/mapDrawer/ZoomLevel.java
+++ src/mapDrawer/ZoomLevel.java
@@ -1,15 +1,91 @@
 package mapDrawer;
 
+/*
+ * Holds seven different levels of zoom, which indicate which type of roads to draw. 
+ * The roadtypes are determined from the percentage of the entire map, which the specified area fills.
+ * Returns a String to use in a XPath search through an XML-file.
+ */
 public enum ZoomLevel {
 	
-	//Which roadType to paint
-	ONE(1), TWO(2), THREE(3), FOUR(4), 
-	FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10);
+	ONE(100.0) {
+
+		@Override
+		protected String XPath() {
+			return "lolONE";
+		}} ,
+		
+	TWO(75.0) {
+
+		@Override
+		protected String XPath() {
+			return "lolTWO";
+		}} ,
+		
+	THREE(56.25) {
+
+		@Override
+		protected String XPath() {
+			return "lolTHREE";
+		}} ,
+		
+	FOUR(42.1875) {
+
+		@Override
+		protected String XPath() {
+			return "lolFOUR";
+		}} ,
+		
+	FIVE(31.640624) {
+
+		@Override
+		protected String XPath() {
+			return "lolFIVE";
+		}} ,
+		
+	SIX(23.73) {
+
+		@Override
+		protected String XPath() {
+			return "lolSIX";
+		}} ,
+		
+	SEVEN(17.79) {
+
+		@Override
+		protected String XPath() {
+			return "LOLSEVEN and BELOW";
+		}}	;	
 	
-	private int value;
+	private double percentageOfEntireMap;
 	
-	private ZoomLevel(int value) 
+	protected abstract String XPath();
+	
+	
+	
+	private ZoomLevel(double percentageOfEntireMap)
 	{
-		this.value = value;
+		this.percentageOfEntireMap = percentageOfEntireMap;
+	}
+	
+	/*
+	 * 
+	 * @return The XPath String to use to search the XML-file for the wanted road types.
+	 */
+	public static String getlevel(double percentageOfEntireMap)
+	{
+		ZoomLevel foundLevel = ONE;
+		
+		for(ZoomLevel zoom : values())
+		{
+			if(zoom.percentageOfEntireMap >= percentageOfEntireMap)
+				foundLevel = zoom;			
+		}
+		
+		return foundLevel.getXPathString();
+	}
+	
+	private String getXPathString()
+	{
+		return this.XPath();
 	}	
 }
