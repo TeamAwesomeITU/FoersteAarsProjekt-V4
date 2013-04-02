eb7eaa89e8f04d2b390deae929d9d9ac8212a36a
Kasper <Kasper.stig1@hotmail.com>
2013-04-02 15:46:35 +0200

    JavaDoc for stack

--- src/mapDrawer/ResizingArrayStack.java
+++ src/mapDrawer/ResizingArrayStack.java
@@ -1,17 +1,35 @@
 package mapDrawer;
 
+/**
+ * Stack that contains the different zooms.
+ * 
+ * @author Kasper Stig Henningsen
+ * @param <Item> Takes an item for the stack.
+ */
 public class ResizingArrayStack<Item>
 {
 	@SuppressWarnings("unchecked")
 	private Item[] a = (Item[]) new AreaToDraw[1]; // stack items
 	private int N = 0; // number of items
 	
+	/**
+	 * Checks wether or not the array is empty.
+	 * @return Boolean for the array.
+	 */
 	public boolean isEmpty() { return N == 0; }
 	
+	/**
+	 * Returns the size of the array.
+	 * @return The size of the array.
+	 */
 	public int size() { return N; }
 	
+	/**
+	 * Resizes the array to avoid loitering.
+	 * @param max The maximum size of the array.
+	 */
 	private void resize(int max)
-	{ // Move stack to a new array of size max.
+	{ 
 		@SuppressWarnings("unchecked")
 		Item[] temp = (Item[]) new AreaToDraw[max];
 		for (int i = 0; i < N; i++)
@@ -19,17 +37,25 @@ public class ResizingArrayStack<Item>
 		a = temp;
 	}
 	
+	/**
+	 * Adds the item to the top of stack.
+	 * @param item The item to be pushed. 
+	 */
 	public void push(Item item)
-	{ // Add item to top of stack.
+	{ 
 		if (N == a.length) resize(2*a.length);
 		a[N++] = item;
 	}
 	
+	/**
+	 * Returns the top item from the stack and deletes it.
+	 * @return The top item in the stack.
+	 */
 	public Item pop()
-	{ // Remove item from top of stack.
+	{
 		if(isEmpty() != true){
 		Item item = a[--N];
-		a[N] = null; // Avoid loitering (see text).
+		a[N] = null; 
 		if (N > 0 && N == a.length/4) resize(a.length/2);
 		return item;
 		}
