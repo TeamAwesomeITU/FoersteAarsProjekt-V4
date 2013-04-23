858958bb09e722c1b95496f88c9aaf2b907b2c17
Kasper <Kasper.stig1@hotmail.com>
2013-04-23 12:21:43 +0200

    PANNING IS DONE! AND SMOOTH! AND AWESOME!

--- src/mapDrawer/drawing/mutators/MapMouseZoomAndPan.java
+++ src/mapDrawer/drawing/mutators/MapMouseZoomAndPan.java
@@ -160,54 +160,47 @@ public class MapMouseZoomAndPan extends MouseAdapter {
 		bigX = coordConverter.pixelToUTMCoordX((int)bigX);
 		smallY = coordConverter.pixelToUTMCoordY((int)smallY);
 		bigY = coordConverter.pixelToUTMCoordY((int)bigY);
-		double panX = (bigX - smallX) /150;
-		double panY = (bigY - smallY) /150;
 		double temp = bigY; bigY = smallY; smallY = temp;
 
 		if(mouseMovedTo.x > mousePressedAt.x)
 		{
-			double tempX = ((mousePressedAt.x - mouseMovedTo.x)/100)*panX;
-			System.out.println("x left: " + tempX);
-			smallX += tempX;
-			bigX += tempX;
-			//bevæger os til venstre
-			
-		}
-		else if(mouseMovedTo.x < mousePressedAt.x)
-		{
-			double tempX = ((mouseMovedTo.x - mousePressedAt.x)/100)*panX;
-			System.out.println("x right: " + tempX);
+			double tempX = coordConverter.pixelToUTMCoordX(mouseMovedTo.x) - coordConverter.pixelToUTMCoordX(mousePressedAt.x);
 			smallX -= tempX;
 			bigX -= tempX;
 			//bevæger os til højre
-			
+		}
+		else if(mouseMovedTo.x < mousePressedAt.x)
+		{
+			double tempX = coordConverter.pixelToUTMCoordX(mousePressedAt.x) - coordConverter.pixelToUTMCoordX(mouseMovedTo.x);
+			smallX += tempX;
+			bigX += tempX;
+			//bevæger os til venstre
 		}
 
 		if(mouseMovedTo.y < mousePressedAt.y)
-		{
-			double tempY = Math.abs(((mouseMovedTo.y - mousePressedAt.y)/100))*panY;
-			System.out.println("y up: " + tempY);
-			smallY += tempY;
-			bigY += tempY;
-			//bevæger os op ad
-			
-		}
-		else if(mouseMovedTo.y > mousePressedAt.y)
-		{
-			double tempY = Math.abs(((mousePressedAt.y - mouseMovedTo.y)/100))*panY;
-			System.out.println("y down: " + tempY);
+		{	
+			double tempY = coordConverter.pixelToUTMCoordY(mouseMovedTo.y) - coordConverter.pixelToUTMCoordY(mousePressedAt.y);
 			smallY -= tempY;
 			bigY -= tempY;
 			//bevæger os ned ad
-			
+		}
+		else if(mouseMovedTo.y > mousePressedAt.y)
+		{
+			double tempY = coordConverter.pixelToUTMCoordY(mousePressedAt.y) - coordConverter.pixelToUTMCoordY(mouseMovedTo.y);
+			smallY += tempY;
+			bigY += tempY;
+			//bevæger os op ad	
 		}
 		
 		try {
 			newArea = new AreaToDraw(smallX, bigX, smallY, bigY, true);
 			mp.repaintMap(newArea);
+			mousePressed(e);
 
 		} catch (NegativeAreaSizeException | AreaIsNotWithinDenmarkException
 				| InvalidAreaProportionsException e1) {
+			e1.printStackTrace();
+			newArea = currentArea;
 		}	
 		
 	}
