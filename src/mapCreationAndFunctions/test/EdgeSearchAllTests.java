f9ffee884907a9dd9ee4dfbb54233738f2c590f2
jnysteen <jespernysteen@gmail.com>
2013-05-09 21:58:36 +0200

    Signed-off-by: jnysteen <jespernysteen@gmail.com>

---
+++ src/mapCreationAndFunctions/test/EdgeSearchAllTests.java
@@ -0,0 +1,12 @@
+package mapCreationAndFunctions.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ EdgeSearchRoadNameAndNumberLetter.class,
+		EdgeSearchRoadNameOnlyTest.class })
+public class EdgeSearchAllTests {
+
+}
