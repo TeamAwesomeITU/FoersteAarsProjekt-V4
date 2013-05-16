285a2108cc19b79e16ee549c13c4526894938e45
Mark <mekl@itu.dk>
2013-05-16 13:48:31 +0200

    Frederik retarderede dijkstra test

--- src/navigation/test/DijsktraSPTest.java
+++ src/navigation/test/DijsktraSPTest.java
@@ -2,6 +2,10 @@ package navigation.test;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 import mapCreationAndFunctions.data.Edge;
@@ -14,7 +18,7 @@ import org.junit.Test;
 public class DijsktraSPTest {
 
 	@Test
-	public void testShortestPath() throws NoRoutePossibleException {
+	public void testShortestPathOne() throws NoRoutePossibleException {
 		EdgeWeightedDigraph graph = new EdgeWeightedDigraph(6);
 		Edge[] testEdges = new Edge[6];
 		Stack<Edge> path = new Stack<Edge>();
@@ -31,7 +35,6 @@ public class DijsktraSPTest {
 		pathToMatch.push(testEdges[1]);
 		for(Edge e: testEdges) {
 			graph.addEdge(e.getFromNode(), e.getiD());
-			System.out.println(e.getFromNode() +", " + e.getiD());
 		}
 		
 		DijkstraSP dip = new DijkstraSP(graph, testEdges[0], testEdges, "Car", "Fastest");
@@ -39,5 +42,63 @@ public class DijsktraSPTest {
 		while(!path.isEmpty()) {
 			assertTrue(path.pop().getiD() == pathToMatch.pop().getiD());
 		}
+	}
+	
+	@Test
+	public void testShortestPathTwo() throws NoRoutePossibleException {
+		Edge[] edgeArray = initialize();
+		EdgeWeightedDigraph graph = new EdgeWeightedDigraph(edgeArray.length);
+		Stack<Edge> path = new Stack<Edge>();
+		Stack<Edge> pathToMatch = new Stack<Edge>();
+		
+		pathToMatch.push(edgeArray[17]);
+		pathToMatch.push(edgeArray[8]);
+		pathToMatch.push(edgeArray[6]);
+		pathToMatch.push(edgeArray[5]);
+		pathToMatch.push(edgeArray[2]);
+		
+		for(Edge e: edgeArray) {
+			graph.addEdge(e.getFromNode(), e.getiD());
+		}
+		
+		DijkstraSP dip = new DijkstraSP(graph, edgeArray[0], edgeArray, "Car", "Shortest");
+		path = (Stack<Edge>) dip.pathTo(edgeArray[17]);
+		while(!path.isEmpty()) {
+			Edge edgeToMatch = pathToMatch.pop();
+			Edge edge = path.pop(); 
+			assertTrue(edge.getFromNode() == edgeToMatch.getFromNode() && edge.getToNode() == edgeToMatch.getToNode());
+		}
+		
+	}
+	
+	public Edge[] initialize() {
+
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("XML/DjikstraTest.txt"), "UTF-8"));
+
+			Edge[] edgeArray = new Edge[22];
+			String line;
+			int counter = 0;
+			while((line = reader.readLine()) != null) 
+			{
+				String[] lineParts = line.split("\\,");
+				
+				Edge edge = new Edge(Integer.parseInt(lineParts[0].trim()), Integer.parseInt(lineParts[1].trim()), Double.parseDouble(lineParts[2].trim()), counter+1, 1,"" , 1, 1, 1, 1, "", "", "", "", 1, 1, 1, 0.0, "", 1, 1, 1);
+
+				edgeArray[counter] = edge;
+				counter++;
+			}
+			
+			for(Edge edges : edgeArray) {
+				System.out.println(edges.getLength());
+			}
+			
+			reader.close();
+			return edgeArray;
+
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		} 
+		return null;
 	}
 }
